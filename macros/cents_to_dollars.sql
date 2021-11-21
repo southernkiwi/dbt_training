af25e5dd48d37a509605010a95b280334af7bcd7
Rowena Winton <rowena.winton@transpower.co.nz>
2021-11-22 09:39:41 +1300

    call cents_to_dollars macro in stg_payments

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,5 +1,3 @@
-{# A basic example for a project-wide macro to cast a column uniformly #}
-
-{% macro cents_to_dollars(column_name, precision=2) -%}
-    ({{ column_name }} / 100)::numeric(16, {{ precision }})
+{% macro cents_to_dollars(column_name, decimal_places=2) -%}
+    round( 1.0 * {{ column_name }} / 100, {{ decimal_places }})
 {%- endmacro %}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,23 +1,21 @@
 with source as (
     
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ source('stripe', 'payment') }}
+   select
+       id as payment_id,
+       orderid as order_id,
+       paymentmethod as payment_method,
+       status,
+       -- amount is stored in cents, convert it to dollars
+       {{ cents_to_dollars('amount', 4) }} as amount,
+       created as created_at
+
+   from {{ source('stripe','payment') }}
 
 ),
 
 renamed as (
 
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars cents_to_dollars(amount)
-         amount as amount
-
+    select *
     from source
 
 )
